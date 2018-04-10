debe275dfb35fb0a189a70539b8d5de8bc8111e9
viclm <xliuming@outlook.com>
2018-04-10 16:32:28 +0800

    rename api

--- test/proxy_spec.js
+++ test/proxy_spec.js
@@ -18,34 +18,51 @@ test.after.always(t => {
   server.close()
 })
 
-const resolveRequestOptions = (options) => {
-  return Object.assign({
-    when: option.wrap(),
-    before: option.wrap(),
-    after: option.wrap(),
-  }, options)
+const allProxyOptions = {
+  get() {}
+}
+
+const createProxyOptions = options => {
+  const resolve = rules => {
+    return rules.map(o => {
+      if (Array.isArray(o)) {
+        return resolve(o)
+      }
+      o = Object.assign({
+        when: option.wrap(),
+        before: option.wrap(),
+        after: option.wrap(),
+      }, o)
+      if (o.url) {
+        o.url = 'http://localhost:' + server.address().port + o.url
+      }
+      return o
+    })
+  }
+  options.rules = resolve(options.rules)
+  return options
 }
 
 test('proxy single request', async t => {
   t.plan(3)
 
-  await proxy({}, {
+  await proxy({}, createProxyOptions({
     rules: [
-      resolveRequestOptions({
-        url: 'http://localhost:' + server.address().port + '/api/xxx',
+      {
+        url: '/api/xxx',
         method: 'get',
-      })
+      }
     ]
-  }).then(result => t.is(result, '[GET]xxx'))
+  }), allProxyOptions).then(result => t.is(result, '[GET]xxx'))
 
-  const error = await t.throws(proxy({}, {
+  const error = await t.throws(proxy({}, createProxyOptions({
     rules: [
-      resolveRequestOptions({
-        url: 'http://localhost:' + server.address().port + '/error_api/xxx',
+      {
+        url: '/error_api/xxx',
         method: 'get',
-      })
+      }
     ]
-  }))
+  }), allProxyOptions))
 
   t.is(error.status, 404)
 
@@ -54,117 +71,117 @@ test('proxy single request', async t => {
 test('merge multiple serial request', async t => {
   t.plan(1)
 
-  await proxy({}, {
+  await proxy({}, createProxyOptions({
     rules: [
-      resolveRequestOptions({
-        url: 'http://localhost:' + server.address().port + '/api/x1',
+      {
+        url: '/api/x1',
         method: 'get',
-      }),
-      resolveRequestOptions({
-        url: 'http://localhost:' + server.address().port + '/api/x2',
+      },
+      {
+        url: '/api/x2',
         method: 'get',
-      })
+      }
     ]
-  }).then(result => t.is(result, '[GET]x2'))
+  }), allProxyOptions).then(result => t.is(result, '[GET]x2'))
 })
 
 test('merge multiple parallel request', async t => {
   t.plan(1)
 
-  await proxy({}, {
+  await proxy({}, createProxyOptions({
     rules: [
       [
-        resolveRequestOptions({
-          url: 'http://localhost:' + server.address().port + '/api/x1',
+        {
+          url: '/api/x1',
           method: 'get',
-        }),
-        resolveRequestOptions({
-          url: 'http://localhost:' + server.address().port + '/api/x2',
+        },
+        {
+          url: '/api/x2',
           method: 'get',
-        })
+        }
       ]
     ]
-  }).then(result => t.deepEqual(result, ['[GET]x1', '[GET]x2']))
+  }), allProxyOptions).then(result => t.deepEqual(result, ['[GET]x1', '[GET]x2']))
 })
 
 test('use when to skip a request', async t => {
   t.plan(1)
 
-  await proxy({}, {
+  await proxy({}, createProxyOptions({
     rules: [
-      resolveRequestOptions({
-        url: 'http://localhost:' + server.address().port + '/api/x1',
+      {
+        url: '/api/x1',
         method: 'get',
-      }),
-      resolveRequestOptions({
-        url: 'http://localhost:' + server.address().port + '/api/x2',
+      },
+      {
+        url: '/api/x2',
         method: 'get',
         when: option.wrap(() => false)
-      })
+      }
     ]
-  }).then(result => t.is(result, '[GET]x1'))
+  }), allProxyOptions).then(result => t.is(result, '[GET]x1'))
 })
 
 test('use before to change the request options', async t => {
   t.plan(1)
 
-  await proxy({}, {
+  await proxy({}, createProxyOptions({
     rules: [
-      resolveRequestOptions({
-        url: 'http://localhost:' + server.address().port + '/api/xxx',
+      {
+        url: '/api/xxx',
         method: 'get',
         before: option.wrap(() => {
           return {
             method: 'post'
           }
         })
-      }),
+      },
     ]
-  }).then(result => t.is(result, '[POST]xxx'))
+  }), allProxyOptions).then(result => t.is(result, '[POST]xxx'))
 })
 
 test('use after to change the result', async t => {
   t.plan(1)
 
-  await proxy({}, {
+  await proxy({}, createProxyOptions({
     rules: [
-      resolveRequestOptions({
-        url: 'http://localhost:' + server.address().port + '/api/xxx',
+      {
+        url: '/api/xxx',
         method: 'get',
         after: option.wrap(() => {
           return 'custom result'
         })
-      }),
+      },
     ]
-  }).then(result => t.is(result, 'custom result'))
+  }), allProxyOptions).then(result => t.is(result, 'custom result'))
 })
 
 test('use fake to custom a request completely', async t => {
   t.plan(1)
 
-  await proxy({}, {
+  await proxy({}, createProxyOptions({
     rules: [
-      resolveRequestOptions({
+      {
         fake: option.wrap(() => {
           return 'fake result'
         })
-      }),
+      },
     ]
-  }).then(result => t.is(result, 'fake result'))
+  }), allProxyOptions).then(result => t.is(result, 'fake result'))
 })
 
 test('use fallback to output defaults when a request broken', async t => {
   t.plan(1)
 
-  await proxy({}, {
+  await proxy({}, createProxyOptions({
     rules: [
-      resolveRequestOptions({
-        url: 'http://localhost:' + server.address().port + '/error_api/xxx',
+      {
+        url: '/error_api/xxx',
         method: 'get',
         fallback: option.wrap(() => {
           return 'fallback result'
         })
-      }),
+      },
     ]
-  }).then(result => t.is(result, 'fallback result'))
+  }), allProxyOptions).then(result => t.is(result, 'fallback result'))
 })
