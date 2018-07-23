7cf6e8ae1d05d400607a7fd3465d35cca969b8fc
viclm <xliuming@outlook.com>
2018-07-23 17:31:46 +0800

    merge rule into config

--- test/config_spec.js
+++ test/config_spec.js
@@ -2,14 +2,49 @@ const test = require('ava')
 const sinon = require('sinon')
 const config = require('../lib/config')
 
-test.serial('set all configs once', t => {
+test('set all configs once', t => {
   config.set({
-    circuitbreaker: true,
-    xxx: 'xxx'
+    x1: 'x1'
   })
 
-  t.is(config.get('circuitbreaker'), true)
-  t.is(config.get('xxx'), 'xxx')
-  t.is(config.circuitbreaker, true)
-  t.is(config.xxx, 'xxx')
+  t.is(config.x1, 'x1')
 })
+
+test('lookup rule by path and method', t => {
+  t.falsy(config.rule('xxx', 'get'))
+
+  config.set('rules', [
+    {
+      path: '/xxx',
+      method: 'get',
+      graph: 'xxx'
+    },
+    {
+      path: '/yyy/*',
+      method: 'post',
+      graph: 'yyy'
+    }
+  ])
+
+  t.is(config.rule('/xxx', 'get'), 'xxx')
+  t.is(config.rule('/yyy/zzz', 'post'), 'yyy')
+  t.falsy(config.rule('/yyy/zzz', 'get'))
+})
+
+// test('plain object argument use a shallow copy, and boolean is used for quick setup', t => {
+//   const obj = {
+//     x1: { x1: 'x1' },
+//     x2: { x2: 'x2' },
+//     x3: { x3: 'x3' },
+//   }
+
+//   config.$extend(obj, {
+//     x1: true,
+//     x2: false,
+//     x3: { x3: 'x', x4: 'xx' }
+//   })
+
+//   t.deepEqual(obj.x1, { x1: 'x1' })
+//   t.is(obj.x2, false)
+//   t.deepEqual(obj.x3, { x3: 'x', x4: 'xx' })
+// })
